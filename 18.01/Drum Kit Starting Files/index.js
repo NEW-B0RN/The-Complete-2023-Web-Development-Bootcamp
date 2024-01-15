ee554ee9cf3c74cef71e9a50854c079749b7c2fc
NEW-BORN <manavbhullar341@gmail.com>
2024-01-15 23:06:23 +0530

    Update drum kit buttons with images

--- 18.01/Drum Kit Starting Files/index.js
+++ 18.01/Drum Kit Starting Files/index.js
@@ -1,4 +1,42 @@
-document.querySelector("button").addEventListener("click",handleclick)
-function handleclick (){
-    alert("I got clicked")
-}
+document.querySelector(".w").addEventListener("click",function  (){
+    var w = new Audio("sounds/tom-1.mp3");
+    w.play()
+})
+
+
+document.querySelector(".a").addEventListener("click",function  (){
+    var w = new Audio("sounds/tom-2.mp3");
+    w.play()
+})
+
+
+document.querySelector(".s").addEventListener("click",function  (){
+    var w = new Audio("sounds/tom-3.mp3");
+    w.play()
+})
+
+
+document.querySelector(".d").addEventListener("click",function  (){
+    var w = new Audio("sounds/tom-4.mp3");
+    w.play()
+})
+
+
+document.querySelector(".j").addEventListener("click",function  (){
+    var w = new Audio("sounds/crash.mp3");
+    w.play()
+})
+
+document.querySelector(".k").addEventListener("click",function  (){
+    var w = new Audio("sounds/kick-bass.mp3");
+    w.play()
+})
+
+
+document.querySelector(".l").addEventListener("click",function  (){
+    var w = new Audio("sounds/snare.mp3");
+    w.play()
+})
+
+
+
