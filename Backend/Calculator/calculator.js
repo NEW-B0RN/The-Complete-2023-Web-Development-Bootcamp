96a45578fcc6bb0abe2b080c3f1094c648da71f5
NEW-BORN <manavbhullar341@gmail.com>
2024-04-15 02:31:12 +0530

    Add BMI calculator functionality to calculator.js

--- Backend/Calculator/calculator.js
+++ Backend/Calculator/calculator.js
@@ -8,6 +8,17 @@ app.get("/", function(req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 
+app.get("/bmicalculator", function(req, res) {
+    res.sendFile(__dirname + "/bmiCalculator.html");
+});
+app.post("/bmicalculator", function(req, res) {
+    var weight = Number(req.body.weight);
+    var height = Number(req.body.height);
+    var bmi = weight / (height * height);
+
+    res.send("Your BMI is " + bmi);
+})
+
 app.post("/", function(req, res){
 
     var num1 = Number(req.body.n1);
@@ -16,7 +27,7 @@ app.post("/", function(req, res){
     var result = num1 + num2;
 
     res.send("The result of the calculation is " + result);
-})
+});
 
 app.listen(3000, function() {
     console.log("Server is running on port 3000");
