05ac18d05c197e9fc0139b18402d65225d367424
NEW-BORN <manavbhullar341@gmail.com>
2024-04-14 05:39:23 +0530

    Add routes for homepage, contact, and about in server.js

--- Backend/3.1 server/server.js
+++ Backend/3.1 server/server.js
@@ -1,6 +1,18 @@
 const express =  require('express');
 const app  = express();
 
+app.get("/", function (request, response){
+    response.send("<h1>Hello World</h1>");
+})
+
+app.get("/contact", function (request , response){
+    response.send("<h2>contact me at : manavbhullar341@gmail.com</h2>")
+})
+
+app.listen("/about", function (request , response){
+    response.send("<h2>My name is Manav Bhullar and I am a web developer</h2>")
+})
+
 app.listen (3000 , function() {
     console.log('Server is running on port 3000');
 });
